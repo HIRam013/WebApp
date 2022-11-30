993c70b55259790e3d77d1b6679d7effc0ec73a4
HIRam013 <100892353+HIRam013@users.noreply.github.com>
2022-11-30 10:12:01 -0600

    Add files via upload

---
+++ json/question.js
@@ -0,0 +1,13 @@
+
+data = [
+		{question: "De que se alimentan los koalas?", answers: {a: "Pasto", b: "Carne", c: "Semillas", d: "Eucalipto" },correctAnswer: "d" },
+		{question: "Cual fue la primera pelicula de Walt Disney?", answers: {a: "La cenicienta", b: "Blancanieves",c: "La dama y el vagabundo", d: "La espada en la piedra" },correctAnswer: "b" },
+		{question: "Cuantas veces parpadea una persona?",answers: {a:"25.000 veces por semana.", b:"15.000 veces por semana.", c:"30.000 veces por semana.", d:"23.000 veces por semana." },correctAnswer: "a" },
+		{question: "Que cantidad de huesos en el cuerpo humano?", answers: {a:"200", b:"204", c:"206", d:"209" },correctAnswer: "c" },
+		{question: "Cual es el oceano mas grande?", answers: {a:"El Oceano Pacifico", b:"El Oceano Atlantico", c:"El Oceano Artico", d:"El Oceano Indico" },correctAnswer: "a" },
+		{question: "Cual es el rio mas largo de Mexico?", answers: {a: "Rio Lerma.", b: "Rio Bravo", c: "Rio Grijalva", d: "Rio Colorado" },correctAnswer: "b" },
+		{question: "A que region pertenecen Tabasco, Quintana Roo y Yucatan?", answers: {a: "Al sureste", b: "Al norte",c: "Al sur", d: "Al noroeste" },correctAnswer: "a" },
+		{question: "Cual es el clima que mas predomina en Mexico?",answers: {a:"Templado subhumedo.", b:"Seco.", c:"Semiseco.", d:"Humedo." },correctAnswer: "a" },
+		{question: "En que area de America se encuentra Mexico?", answers: {a:"En Sur America", b:"En Centro America", c:"En Norte America", d:"Ninguno" },correctAnswer: "c" },
+		{question: "Donde se ubica la piramide mas grande de Mexico?", answers: {a:"Veracruz", b:"Yucatan", c:"Guadalajara", d:"Chiapas" },correctAnswer: "d" }
+]
